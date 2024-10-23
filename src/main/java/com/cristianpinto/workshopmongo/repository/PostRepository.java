a45d3fb7fd21ef92cba363b9a16cea725360af1e
Cristian554 <cristianpinto256@gmail.com>
2024-10-23 18:39:19 -0300

    Consulta simples com query methods

--- src/main/java/com/cristianpinto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cristianpinto/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,7 @@
 package com.cristianpinto.workshopmongo.repository;
 
+import java.util.List;
+
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,6 +10,6 @@ import com.cristianpinto.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{// Conectar banco de dados do Mongo
 
-	
+	List<Post> findByTitleContainingIgnoreCase(String text); // Ignore case para ignorar o formato das letras
 
 }
