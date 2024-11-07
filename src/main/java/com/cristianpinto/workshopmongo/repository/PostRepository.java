15c4f46e7cb439c677d2cd2093489d935e362c74
Cristian554 <cristianpinto256@gmail.com>
2024-11-07 08:33:53 -0300

    Consulta com vários critérios

--- src/main/java/com/cristianpinto/workshopmongo/repository/PostRepository.java
+++ src/main/java/com/cristianpinto/workshopmongo/repository/PostRepository.java
@@ -1,5 +1,6 @@
 package com.cristianpinto.workshopmongo.repository;
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
@@ -11,9 +12,12 @@ import com.cristianpinto.workshopmongo.domain.Post;
 @Repository
 public interface PostRepository extends MongoRepository<Post, String>{// Conectar banco de dados do Mongo
 	
-	@Query("{'title: { $regex: ?0, $options: 'i' } }") // Ignorar a verificação do case
+	@Query("{ 'title': { $regex: ?0, $options: 'i' } }") // Ignorar a verificação do case, Query endPoint de consulta
 	List<Post> searchTitle(String text);
 
 	List<Post> findByTitleContainingIgnoreCase(String text); // Ignore case para ignorar o formato das letras
+	
+	@Query("{ $and: [ { date: {$gte: ?1} }, { date: { $lte: ?2} } , { $or: [ { 'title': { $regex: ?0, $options: 'i' } }, { 'body': { $regex: ?0, $options: 'i' } }, { 'comments.text': { $regex: ?0, $options: 'i' } } ] } ] }")// Utiliza-se and pois seria entre a data max. E a data Min. 
+	List<Post> fullSearch(String text, Date minDate,Date maxDate);
 
 }
--- src/main/java/com/cristianpinto/workshopmongo/services/PostService.java
+++ src/main/java/com/cristianpinto/workshopmongo/services/PostService.java
@@ -1,13 +1,18 @@
 package com.cristianpinto.workshopmongo.services;
 
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.cristianpinto.workshopmongo.domain.Post;
 import com.cristianpinto.workshopmongo.repository.PostRepository;
+import com.cristianpinto.workshopmongo.resources.util.URL;
 import com.cristianpinto.workshopmongo.services.exception.ObjectNotFoundException;
 
 @Service
@@ -24,4 +29,9 @@ public class PostService {
 	public List<Post> findByTitle(String text){
 		return repo.findByTitleContainingIgnoreCase(text);
 	}
+	public List<Post> fullSearch(String text,Date minDate,Date maxDate){
+		maxDate = new Date(maxDate.getTime() + 24 * 60 * 60 * 1000);
+		return repo.fullSearch(text, minDate, maxDate);
+	}
+	
 }
